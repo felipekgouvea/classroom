950b54bc836d59352459e2f1e1edfb17bc5d3708
felipekgouvea <felipekinupesg@hotmail.com>
2023-07-29 15:03:19 -0300

    Create Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ClassRoom } from './pages/ClassRoom'
+
+function App() {
+  return (
+    <div>
+      <ClassRoom />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/ClassRoom.tsx
@@ -0,0 +1,9 @@
+import { Header } from '../components/Header'
+
+export const ClassRoom = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
